d2864d6918e4299e73b827b84796592d00800a81
mmingx <88543616+mmingx@users.noreply.github.com>
2022-11-09 21:21:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,32 +180,6 @@ def get_to_generate_issues(repo, dir_name, issue_number=None):
     return to_generate_issues
 
 
-def generate_rss_feed(repo, filename, me):
-    generator = FeedGenerator()
-    generator.id(repo.html_url)
-    generator.title(f"RSS feed of {repo.owner.login}'s {repo.name}")
-    generator.author(
-        {"name": os.getenv("GITHUB_NAME"), "email": os.getenv("GITHUB_EMAIL")}
-    )
-    generator.link(href=repo.html_url)
-    generator.link(
-        href=f"https://raw.githubusercontent.com/{repo.full_name}/main/{filename}",  
-        rel="self",
-    )
-    for issue in repo.get_issues():
-        if not issue.body or not is_me(issue, me) or issue.pull_request:
-            continue
-        item = generator.add_entry(order="append")
-        item.id(issue.html_url)
-        item.link(href=issue.html_url)
-        item.title(issue.title)
-        item.published(issue.created_at.strftime("%Y-%m-%dT%H:%M:%SZ"))
-        for label in issue.labels:
-            item.category({"term": label.name})
-            body = ''.join(c for c in issue.body if _valid_xml_char_ordinal(c))
-            item.content(CDATA(marko.convert(body)), type="html")
-    generator.atom_file(filename)
-
 
 def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     user = login(token)
@@ -216,7 +190,6 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     for func in [add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
 
-    generate_rss_feed(repo, "feed.xml", me)
     to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
 
     # save md files to backup folder
